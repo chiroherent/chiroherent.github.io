b0bc39032bf190b73e7080c52a69aad1a58cb45b
DraakjeYoblama <draakjeyoblama@gmail.com>
2022-05-03 17:32:39 +0200

    Verbeterde leiding aanpassen met javascript

--- README.md
+++ README.md
@@ -3,15 +3,14 @@
 ## Hoe pas ik de website aan?
 ### Een gids voor arme Chiro Herent leiding die deze taak hebben gekregen.
 #### (Momenteel is deze gids niet af en ook niet juist, woeps, ik heb het geschreven zoals het zou moeten worden in de toekomst)
-1. Pas de lijst met leiding elk jaar aan.
+1. Pas de lijst met leiding elk jaar aan. Dat pas je aan in het document [leiding.js](chiroherent.github.io/leiding.js), daar vind je ook meer uitleg.
 2. Werk zo generic mogelijk:
 	- Zorg dat alle pagina's zo dynamisch mogelijk zijn voor verschillende groottes van scherm.
 	- Zorg dat de bestanden duidelijk blijven voor mensen die na jou de website moeten aanpassen.
 	- Tekst gaat tussen de <main> tags. Daar gebruik je voornamelijk \<h3>, \<p> en \<img> voor resp. titels, tekst en afbeeldingen.
-	- Pas de opmaak liefst aan in de CSS file ipv bij de html.
-	- Geef liever een class dan een id aan een html object, want van een id kan je maar 1 per document hebben. Soms is een class geen optie en dan is een id wel mogelijk. Geef die class dan ook een naam die duidelijk is.
+	- Pas de opmaak liefst aan in de CSS file ipv bij de html. Tenzij het maar één keer/maar tijdelijk nodig is = niet handig voor hergebruik, dan mag je style="" gebruiken.
+	- Geef liever een class dan een id aan een html object, want van een id kan je maar 1 per document hebben. Soms is een class geen optie, zoals bij javascript geeft dat veel gedoe. De naam moet in ieder geval zinvol/duidelijk zijn.
 	- Zorg dat de CSS zoveel mogelijk herbruikbaar is in gelijkaardige situaties.
-	- Als je weet dat je iets maar 1 keer (tijdelijk) nodig gaat hebben kan je het wel als style="" toevoegen in de html. 
 	- Er is een template om nieuwe pagina's mee te maken
 	- Gebruik var(--kleurnaam) om een vaak gebruikte (chiro)kleur in de css te gebruiken. De lijst met die kleuren staat bovenaan in style.css. Je kan in die lijst ook makkelijk de primaire en secundaire kleur aanpassen.
 3. Design in de eerste plaats voor telefoons en verander de dingen voor desktop daarna met een @media query. (momenteel is het dus omgekeerd, maar dat moet nog gefixt)
@@ -26,7 +25,6 @@
   - Zorgen dat de website beter werkt op smartphones
   - Het menu fixen, want janky
   - Uiteindelijk zou alle html mooi en hetzelfde opgemaakt moeten zijn, en dan de css opnieuw overzichtelijk opmaken met telefoons als primaire gedachte en dan computers secundair, dus @media voor desktop.
-- Eventueel zorgen dat de lijst met leiding met een spreadsheet kan aangepast worden en als dat werkt misschien ook de andere pagina's?
 - Hogere kwaliteit foto's erop zetten en misschien een nieuw logo
 - Nieuwe figuur met alle groepen + hun leeftijden maken
 - probably nog dingen die ik vergeten ben
--- leiding.js
+++ leiding.js
@@ -1,4 +1,6 @@
-function nieuweLeiding(naam, email, telefoon) {
+/* Van dit deel vanboven moet je afblijven tenzij je weet wat je aan het doen bent */
+
+function leiding(naam, email, telefoon) {
 	if (telefoon != null) {
 		return '<dt><strong>'+naam+'</strong></dt><dd><a href="mailto:'+email+'">'+email+'</a></dd><dd>'+telefoon+'</dd>';
   } else {
@@ -7,5 +9,92 @@ function nieuweLeiding(naam, email, telefoon) {
 }
 
 
+/* vvvvvvvvvvvvvvvvvvvvvv VANAF HIER PAS JE AAN vvvvvvvvvvvvvvvvvvvvvv
 
-document.getElementById("leiding").innerHTML = nieuweLeiding();
+
+Hieronder staat het deel dat je mag aanpassen, gewoon dat deel tussen de haakjes
+Uiteindelijk zou het er moeten uitzien zoals hier:
+
+document.getElementById("groepsnaam").innerHTML = leiding("Naam", "email", "telefoonnummer")+leiding("Naam2", "email2", null);
+
+(als je geen telefoonnummer wil invullen zet je null zonder aanhalingstekens, als het nodig is kan daar ook iets anders dan een telefoonnummer)
+
+Voor meerdere leiding moet je gewoon leiding(blablabla)+leiding(blablabla2)+leiding(blablabla3)+...
+
+Om het overzichtelijk te houden gebruik je best enters tussen verschillende leiding.
+*/
+
+document.getElementById("hoofdleiding").innerHTML = 
+	leiding("Katoo Janssens", "katoojanssens@gmail.com", "0497 38 96 30")
+	+ leiding("Felix Soers", "felixsoers@gmail.com", "0479 81 42 06")
+	+ leiding("Heleen Stessens", "heleen.stessens", "0491 18 97 95")
+;
+
+document.getElementById("vb").innerHTML = 
+	leiding("Lore Houben", "lorehouben@hotmail.com", "0471 11 97 98")
+;
+
+document.getElementById("kiekeboes").innerHTML = 
+	leiding("Katoo Janssens", "katoojanssens@gmail.com", null)
+	+ leiding("Anne-Sophie Hons", "annesophie.hons@hotmail.com", null)
+	+ leiding("Luna Neyens", "luna.neyens@hotmail.com", null)
+	+ leiding("Lukas Verboven", "lukas.verboven@gmail.com", null)
+	+ leiding("Yagon Hannot", "yagonhannotgus@gmail.com", null)
+;
+
+document.getElementById("speelclub").innerHTML = 
+	leiding("Hasse Casaer", "hasse.casaer@gmail.com", null)
+	+ leiding("Pixie Neyens", "pixie.neyens@hotmail.com", null)
+	+ leiding("Douwe Somers", "douwesomers@live.be", null)
+	+ leiding("Rens Deschryver", "rens.deschryver@gmail.com", null)
+	+ leiding("Senne Beunen", "senne.beunen@gmail.com", null)
+;
+
+document.getElementById("kwikken").innerHTML = 
+	leiding("Esmée Devlieger", "esmeedevlieger@gmail.com", null)
+	+ leiding("Mette Couvreur", "mette.couvreur@hotmail.com", null)
+	+ leiding("Niels Malliet", "niels.malliet@gmail.com", null)
+;
+
+document.getElementById("rakkers").innerHTML = 
+	leiding("Phil Huybens", "phil.huybens@outlook.com", null)
+	+ leiding("Alizée Jacobs", "alizee.jacobs@hotmail.be", null)
+	+ leiding("Gijs Puttemans", "gijs.puttemans20@gmail.com", null)
+;
+
+document.getElementById("tippers").innerHTML = 
+	leiding("Elle Wouters", "ellewouters@gmail.com", null)
+	+ leiding("Jur Melio", "jur.melio@hotmail.com", null)
+	+ leiding("Daan Cornet", "cornet.daanbm@gmail.com", null)
+;
+
+document.getElementById("toppers").innerHTML = 
+	leiding("Wannes Dirix", "wannes.dirix@gmail.com", null)
+	+ leiding("Janne Boogaerts", "janneboogaerts@gmail.com", null)
+	+ leiding("Elgar Suy", "elgarsvb@gmail.com", null)
+;
+
+document.getElementById("tiptiens").innerHTML = 
+	leiding("Heleen Stessens", "heleen.stessens@gmail.com", null)
+	+ leiding("Pauline Poplemont", "paulinepoplemont@live.be", null)
+	+ leiding("Romee Coelst", "romeecoelst@hotmail.com", null)
+;
+
+document.getElementById("kerels").innerHTML = 
+	leiding("Robbe Resseler", "robberesseler@hotmail.com", null)
+	+ leiding("Dries Pollers", "dpollers@gmail.com", null)
+	+ leiding("Wouter Capoen", "capoenwouter@gmail.com", null)
+;
+
+document.getElementById("aspimeisjes").innerHTML = 
+	leiding("Tine Vandenberghe", "tinevandenberghe0@gmail.com", null)
+	+ leiding("Kirsten Capoen", "kirsten.capoen@telenet.be", null)
+	+ leiding("Luca Jacobs", "lucajacobs73@gmail.com", null)
+;
+
+document.getElementById("aspijongens").innerHTML = 
+	leiding("Felix Soers", "felixsoers@gmail.com", null)
+	+ leiding("Chiel Habils", "chiel.habils@hotmail.com", null)
+	+ leiding("Xander De Kay", "Xander.dekay@gmail.com", null)
+;
+
