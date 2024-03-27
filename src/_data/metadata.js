c36218cab09d7d73a187332cd3751addfd4b1838
DraakjeYoblama <draakjeyoblama@gmail.com>
2024-03-27 10:12:58 +0100

    Leiding lijst omzetten naar eleventy
    De lijst met leiding is nu opgeslagen als yaml, dus makkelijker om te lezen en aan te passen

---
+++ src/_data/metadata.js
@@ -0,0 +1,6 @@
+module.exports = {
+	title: "Chiro Herent",
+	url: "https://www.chiroherent.be/",
+	language: "nl-BE",
+	description: "Chiro Herent is een organisatie door, maar vooral voor jongeren, gelegen in de Broekveldstraat 33. Het is een plaats waar de kinderen zich kunnen uitleven, spelen en ravotten, knutselen en origineel zijn. Maar het is tevens een plaats waar groepsgeest, samenhorigheid en eendracht primeren. Voor elke leeftijdscategorie van 6 tot 18 jaar is er een groep voorzien, er is dus voor ieder wat wils."
+}
